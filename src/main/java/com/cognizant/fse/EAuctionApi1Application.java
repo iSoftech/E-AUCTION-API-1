67efe418f41cf2cca4d053a5bc65620adccce7c2
iSoftech <m.mohamedyusuff@gmail.com>
2021-11-19 00:14:33 +0800

    SpringBoot project setup - initial commit

---
+++ src/main/java/com/cognizant/fse/EAuctionApi1Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.fse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAuctionApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAuctionApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/fse/EAuctionApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.fse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAuctionApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
